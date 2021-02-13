ebdffded9089bc5db52d55f81e65b98b7b6d4685
emrizki <emrizkidev@gmail.com>
2021-02-13 15:53:36 +0700

    commit again

--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,9 @@ const userRouter = require('./userRouter')
 const weatherRouter = require('./weatherRouter')
 const { authentication } = require('../middlewares')
 
+router.get('/', (req, res) => {
+  res.json({message: "Welcom to Fancy Todo Reborn"})
+})
 router.use('/users', userRouter)
 router.use(authentication)
 router.use('/todos', todoRouter)
